d300c17be4d72b351e06542b9a0ecdf42fe26e67
nadakarim <36544664+nadakarim@users.noreply.github.com>
2018-02-22 12:33:48 +0200

    Q1

---
+++ blinking_led.ino
@@ -0,0 +1,14 @@
+int pin = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (pin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite (pin, HIGH);
+delay (500);
+digitalWrite (pin, LOW);
+delay (1500);
+}
