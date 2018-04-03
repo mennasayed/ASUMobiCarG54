c313543f6b268b1820fe7ea8de01554bbd53032e
mennasalah017 <36541376+mennasalah017@users.noreply.github.com>
2018-04-03 19:14:29 +0200

    Add files via upload

---
+++ sketch_apr03a2.ino
@@ -0,0 +1,32 @@
+
+#include <Process.h>
+
+void setup() {
+  SerialUSB.begin(9600);  // initialize serial communication
+  while (!SerialUSB);     // do nothing until the serial monitor is opened
+
+  SerialUSB.println("Starting bridge...\n");
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Bridge.begin();  // make contact with the linux processor
+  digitalWrite(13, HIGH);  // Led on pin 13 turns on when the bridge is ready
+
+  delay(2000);  // wait 2 seconds
+}
+
+void loop() {
+  Process wifiCheck;  // initialize a new process
+
+  wifiCheck.runShellCommand("/usr/bin/pretty-wifi-info.lua");  // command you want to run
+
+  // while there's any characters coming back from the
+  // process, print them to the serial monitor:
+  while (wifiCheck.available() > 0) {
+    char c = wifiCheck.read();
+    SerialUSB.print(c);
+  }
+
+  SerialUSB.println();
+
+  delay(5000);
+}
