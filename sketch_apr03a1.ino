85b0d853955d3866719ec94fcc5ead2b2633374e
mennasalah017 <36541376+mennasalah017@users.noreply.github.com>
2018-04-03 19:10:12 +0200

    Add files via upload

---
+++ sketch_apr03a1.ino
@@ -0,0 +1,35 @@
+#include <Console.h>
+
+String name;
+
+void setup() {
+  // Initialize Console and wait for port to open:
+  Bridge.begin();
+  Console.begin();
+
+  // Wait for Console port to connect
+  while (!Console);
+
+  Console.println("Hi, what's your name?");
+}
+
+void loop() {
+  if (Console.available() > 0) {
+    char c = Console.read(); // read the next char received
+    // look for the newline character, this is the last character in the string
+    if (c == '\n') {
+      //print text with the name received
+      Console.print("Hi ");
+      Console.print(name);
+      Console.println("! Nice to meet you!");
+      Console.println();
+      // Ask again for name and clear the old name
+      Console.println("Hi, what's your name?");
+      name = "";  // clear the name string
+    } else {     // if the buffer is empty Cosole.read() returns -1
+      name += c; // append the read char from Console to the name string
+    }
+  } else {
+    delay(100);
+  }
+}
