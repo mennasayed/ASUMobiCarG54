a9facb6f67351bb715f96eb72a5ef65e99993757
nadakarim <36544664+nadakarim@users.noreply.github.com>
2018-07-05 23:30:06 +0200

    Add files via upload

---
+++ Tilt_Sensor_Interfacing.ino
@@ -0,0 +1,16 @@
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, INPUT);
+}
+void loop() {
+  if (digitalRead(4) == 1)
+  {
+  digitalWrite(2, HIGH);   
+  digitalWrite(3, HIGH);
+  delay(300);                       
+  digitalWrite(2, LOW);    
+  digitalWrite(3, LOW); 
+  delay(300);  
+  }
+}
