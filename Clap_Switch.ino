b3a0f959ca681a965e2b2fa26dd824b7f2955428
nadakarim <36544664+nadakarim@users.noreply.github.com>
2018-07-06 00:27:21 +0200

    Add files via upload

---
+++ Clap_Switch.ino
@@ -0,0 +1,15 @@
+const int analogInPin = A0;  // Analog input pin 0
+int sensorValue = 0;   
+void setup() 
+{
+ DDRD = 0xFF;
+}
+void loop() 
+{
+ sensorValue = analogRead(analogInPin);    //read ADC value on channel 0
+ if(sensorValue>60)
+ {
+  PORTD ^=(1<<7); //If there is a peak toggle the LED on and OFF on pin7.
+  delay(250);
+  }
+}
