811c17dd74754210938419a5cce7b3df216393f9
YasmeenMedhat <36551202+YasmeenMedhat@users.noreply.github.com>
2018-07-05 22:28:04 +0200

    Add files via upload

---
+++ Rectangle_pt.3.ino
@@ -0,0 +1,22 @@
+void rectangle () 
+{
+      for(int i=0;i<4;i++){
+forward();
+delay (1000);
+Stop();
+delay(1000);
+right();
+delay (900);
+Stop();
+delay(1000);}
+}
+    
+    void loop() 
+    {
+
+  // put your main code here, to run repeatedly:
+  rectangle ();
+  delay(5000);
+  Stop();
+  delay(5000);
+    }
