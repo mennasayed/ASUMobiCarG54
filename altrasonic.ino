272e17fd3ecdc469ddbf9ad5699d4a5e0c44ea37
Manalmohmed <36531409+Manalmohmed@users.noreply.github.com>
2018-07-06 00:42:25 +0200

    Add files via upload

---
+++ altrasonic.ino
@@ -0,0 +1,40 @@
+const int trigPin = 2;
+const int echoPin = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long duration, inches, cm;
+  pinMode(trigPin, OUTPUT);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(100);
+  }
+long microsecondsToCentimeters(long microseconds)
+{
+
+  return microseconds / 74 / 2;
+}
+long microsecondsToCentimeters(long microseconds)
+{
+    return microseconds / 29 / 2;
+}
+}
