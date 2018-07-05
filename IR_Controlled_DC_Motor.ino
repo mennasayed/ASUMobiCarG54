80c3bc1d935c8f058ceeabfd5b4d016a01d4f108
nadakarim <36544664+nadakarim@users.noreply.github.com>
2018-07-06 00:10:30 +0200

    Add files via upload

---
+++ IR_Controlled_DC_Motor.ino
@@ -0,0 +1,15 @@
+void setup() {
+pinMode(2,INPUT);
+pinMode(7,OUTPUT);
+Serial.begin(9600);
+}
+void loop() {
+  if (digitalRead(2) == 1)
+{
+  Serial.println(digitalRead(2));
+  digitalWrite(7,HIGH);
+}  
+else{
+    digitalWrite(7,LOW);
+    }
+}
