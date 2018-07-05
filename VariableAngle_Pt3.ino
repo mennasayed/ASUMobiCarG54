3340f624a0bcdac09627c0b7f0b29a8502913520
YasmeenMedhat <36551202+YasmeenMedhat@users.noreply.github.com>
2018-07-05 22:26:17 +0200

    Add files via upload

---
+++ VariableAngle_Pt3.ino
@@ -0,0 +1,21 @@
+void Stop ()
+{  
+  digitalWrite(motorA1,LOW);
+ digitalWrite(motorA2,LOW);
+ digitalWrite(motorB1,LOW);
+ digitalWrite(motorB2,LOW);
+ }
+
+  void right ()
+ {  
+  digitalWrite(motorA1,LOW);
+ digitalWrite(motorA2,HIGH);
+ digitalWrite(motorB1,HIGH);
+ digitalWrite(motorB2,LOW);
+ analogWrite (enA,0);
+ analogWrite (enB,120);}
+ oid angle2 (int z){float t2 = (z/360)* 2.387;
+   t2=t2*1000;
+right();
+  delay (500);
+  }
