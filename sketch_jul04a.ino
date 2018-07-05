31f4ab8ad5cf542f5f5fc45a8bce688d84d1115d
Manalmohmed <36531409+Manalmohmed@users.noreply.github.com>
2018-07-06 01:12:58 +0200

    Add files via upload

---
+++ sketch_jul04a.ino
@@ -0,0 +1,38 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ void infinity ()
+{
+  delay (2000);
+    digitalWrite(motorA1,HIGH);
+ digitalWrite(motorA2,LOW);
+ digitalWrite(motorB1,HIGH);
+ digitalWrite(motorB2,LOW);
+analogWrite (enA,100);
+ analogWrite (enB,200);
+ delay(4500/2);
+ 
+  digitalWrite(motorA1,HIGH);
+ digitalWrite(motorA2,LOW);
+ digitalWrite(motorB1,HIGH);
+ digitalWrite(motorB2,LOW);
+analogWrite (enA,200);
+ analogWrite (enB,100);
+   delay(3600);
+    digitalWrite(motorA1,HIGH);
+ digitalWrite(motorA2,LOW);
+ digitalWrite(motorB1,HIGH);
+ digitalWrite(motorB2,LOW);
+analogWrite (enA,100);
+ analogWrite (enB,200);
+ delay(4800/2);
+ Stop();
+   delay(5000);
+  }
+  
+  
+}
