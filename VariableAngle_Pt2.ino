341ba1faf83edeb1c3de4aa5e144f094e8b2f810
YasmeenMedhat <36551202+YasmeenMedhat@users.noreply.github.com>
2018-07-06 02:54:39 +0200

    Add files via upload

---
+++ VariableAngle_Pt2.ino
@@ -0,0 +1,12 @@
+void setup()
+{
+  // put your setup code here, to run once:
+pinMode(motorA1,OUTPUT);
+pinMode(motorA2,OUTPUT);
+pinMode(motorB1,OUTPUT);
+pinMode(motorB2,OUTPUT);
+
+analogWrite (enA, 250);
+analogWrite (enB,250);
+
+}
