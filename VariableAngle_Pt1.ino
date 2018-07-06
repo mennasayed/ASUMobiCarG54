f5b0e3feb645409289046f1125b7d1300a0dc76d
YasmeenMedhat <36551202+YasmeenMedhat@users.noreply.github.com>
2018-07-06 02:54:21 +0200

    Add files via upload

---
+++ VariableAngle_Pt1.ino
@@ -0,0 +1,15 @@
+int motorA1=2;
+int motorA2=3;
+int motorB1=4;
+int motorB2=5;
+int enA =9;
+int enB =10; 
+int i=0;int j=0;
+int data1,data2,data3;
+int sensorA = 6;
+int sensorB = 7;
+int sensorC = 8;
+int carspeed;
+float T;
+int d;
+int theta;
