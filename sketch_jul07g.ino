fbfc326d1baf162a4657c7012004ec4a65bd16f8
mennasalah017 <36541376+mennasalah017@users.noreply.github.com>
2018-07-07 02:30:05 +0200

    angle4

---
+++ sketch_jul07g.ino
@@ -0,0 +1,45 @@
+void back (){ 
+  digitalWrite(motorA1,LOW);
+ digitalWrite(motorA2,HIGH);
+ digitalWrite(motorB1,LOW);
+ digitalWrite(motorB2,HIGH);
+digitalWrite (enA , HIGH);
+digitalWrite (enB , HIGH);}
+
+//=======================================================
+
+//========================================================
+ void right ()
+ { digitalWrite(motorA1,LOW);
+ digitalWrite(motorA2,HIGH);
+ digitalWrite(motorB1,HIGH);
+ digitalWrite(motorB2,LOW);
+analogWrite (enA,200);
+ analogWrite (enB,200);}
+
+  void right2 ()
+ { digitalWrite(motorA1,LOW);
+ digitalWrite(motorA2,HIGH);
+ digitalWrite(motorB1,HIGH);
+ digitalWrite(motorB2,LOW);
+analogWrite (enA,0);
+ analogWrite (enB,150);}
+
+void left (){ digitalWrite(motorA1,HIGH);
+ digitalWrite(motorA2,LOW);
+ digitalWrite(motorB1,LOW);
+ digitalWrite(motorB2,HIGH);
+analogWrite (enA,200);
+ analogWrite (enB,200);}
+
+ void left2 (){ digitalWrite(motorA1,HIGH);
+ digitalWrite(motorA2,LOW);
+ digitalWrite(motorB1,LOW);
+ digitalWrite(motorB2,HIGH);
+analogWrite (enA,150);
+ analogWrite (enB,0);}
+
+void Stop (){  digitalWrite(motorA1,LOW);
+ digitalWrite(motorA2,LOW);
+ digitalWrite(motorB1,LOW);
+ digitalWrite(motorB2,LOW);}
