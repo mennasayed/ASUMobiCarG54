be599091c03dab56e8aef67420cfc38f3d7fa90a
mennasalah017 <36541376+mennasalah017@users.noreply.github.com>
2018-07-07 02:00:30 +0200

    Add files via upload

---
+++ sketch_jul07ba.ino
@@ -0,0 +1,31 @@
+
++Serial.print("data1  =  ");
++Serial.println(data1);
++Serial.print("data2  =  ");
++Serial.println(data2);
++Serial.print("data3  =  ");
++Serial.println(data3);
++if ((data1==HIGH)&&(data2==LOW)&&(data3==HIGH))forward2();
++if ((data1==LOW)&&(data2==HIGH)&&(data3==HIGH))left2();
++if ((data1==HIGH)&&(data2==HIGH)&&(data3==LOW))right2();
++if ((data1==LOW)&&(data2==LOW)&&(data3==HIGH))left2();
++if ((data1==HIGH)&&(data2==LOW)&&(data3==LOW))right2();
++}
++}
++
++ if (value== 'D')
++ {
++  int x = hat();
++  Serial.println(x);
++  distanceF (x);
++  }
++ if (value== 'A')
++ {
++   int x = hat();
++  Serial.println(x);
++  angle (x);
++  }
++if (value== 'Z')
++{
++  
++  delay(5000);
