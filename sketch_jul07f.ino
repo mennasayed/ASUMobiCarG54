e28260bd43903b80e646a61c3b2fda33ea0957df
mennasalah017 <36541376+mennasalah017@users.noreply.github.com>
2018-07-07 02:27:59 +0200

    angle3

---
+++ sketch_jul07f.ino
@@ -0,0 +1,27 @@
+void angle (int y){
+float t2 = (y/360)* T;
+right();
+  delay (t2);
+forward ();
+delay(8000);Stop();
+ }
+ void angle2 (int z){float t2 = (z/360)* T;
+right();
+  delay (t2);}
+      
+ void forward ()
+ {   digitalWrite(motorA1,HIGH);
+ digitalWrite(motorA2,LOW);
+ digitalWrite(motorB1,HIGH);
+ digitalWrite(motorB2,LOW);
+ analogWrite (enA,255);
+ analogWrite (enB,255);}
+
+  void forward2 ()
+ { digitalWrite(motorA1,HIGH);
+ digitalWrite(motorA2,LOW);
+ digitalWrite(motorB1,HIGH);
+ digitalWrite(motorB2,LOW);
+ analogWrite (enA,160);
+ analogWrite (enB,160);}
+
