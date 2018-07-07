3aed7b7531e3f4c6266df9d3de7216fd778df830
mennasalah017 <36541376+mennasalah017@users.noreply.github.com>
2018-07-07 02:26:09 +0200

    angle2

--- sketch_jul07e.ino
+++ sketch_jul07e.ino
@@ -6,8 +6,8 @@ int echoPin = 12;
 long duration, cm;
 void setup() {
 
- // put your setup code here, to run once:
 
+ 
 Serial.begin(9600);
 HC06.begin(9600);
 
@@ -19,26 +19,3 @@ pinMode(motorB2,OUTPUT);
   pinMode(echoPin, INPUT);
 analogWrite (enA, 250);
 analogWrite (enB,250);
-
-void rectangle () {
-      for(int i=0;i<4;i++){
-forward();
-delay (1000);
-delay(1000);
-right();
-delay (900);
-Stop();
-delay(1000);}
-    }
-+void circle ()
-{
-  digitalWrite(motorA1,HIGH);
-}
- digitalWrite(motorA2,LOW);
- digitalWrite(motorB1,HIGH);
- digitalWrite(motorB2,LOW);
-analogWrite (enA,160);
- analogWrite (enB,250);
- delay(5000*2);
-  
-  }
