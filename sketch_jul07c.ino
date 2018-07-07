3886b3fb4275658c997bda09d7f79517b642c830
mennasalah017 <36541376+mennasalah017@users.noreply.github.com>
2018-07-07 02:21:15 +0200

    Dis2

--- sketch_jul07c.ino
+++ sketch_jul07c.ino
@@ -1,12 +1,38 @@
+int d;
+int theta;
 
-void circle ()
-{
-   digitalWrite(motorA1,HIGH);
- digitalWrite(motorA2,LOW);
- digitalWrite(motorB1,HIGH);
-digitalWrite(motorB2,LOW);
-analogWrite (enA,160);
- analogWrite (enB,250);
- delay(5000*2);
-  
-  }
+int trigPin = 11;    
+int echoPin = 12;    
+long duration, cm;
+void setup() {
+
+ // put your setup code here, to run once:
+
+Serial.begin(9600);
+HC06.begin(9600);
+
+pinMode(motorA1,OUTPUT);
+pinMode(motorA2,OUTPUT);
+pinMode(motorB1,OUTPUT);
+pinMode(motorB2,OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+analogWrite (enA, 250);
+analogWrite (enB,250);
+
+
+}
++void distanceF (int x){
++  float t = x/20;
++  t=t*1000;
++ forward();
++ delay (t);
++Stop();
++  }
++ void angle (int y){
++float t2 = (y/360)* T;
++right();
++  delay (t2);
++forward ();
++delay(8000);Stop();
++ }
