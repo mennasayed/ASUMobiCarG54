ab02a6aa8cae844e590dd583ddcfed1856c56026
mennasalah017 <36541376+mennasalah017@users.noreply.github.com>
2018-07-07 02:05:28 +0200

    part of shapes

---
+++ sketch_jul07c.ino
@@ -0,0 +1,12 @@
+
+void circle ()
+{
+   digitalWrite(motorA1,HIGH);
+ digitalWrite(motorA2,LOW);
+ digitalWrite(motorB1,HIGH);
+digitalWrite(motorB2,LOW);
+analogWrite (enA,160);
+ analogWrite (enB,250);
+ delay(5000*2);
+  
+  }
